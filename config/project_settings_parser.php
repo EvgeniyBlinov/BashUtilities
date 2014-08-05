172de421ba37e6a80f985a79e8c48021f25f7f05
Blinov my nout <evgeniy_blinov@mail.ru>
2014-08-06 01:21:22 +0300

    Add license

--- config/project_settings_parser.php
+++ config/project_settings_parser.php
@@ -1,4 +1,16 @@
 <?php
+/**
+ * Config parser.
+ * Copyright (c) 2014 Evgeniy Blinov (http://blinov.in.ua/)
+ * 
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ * 
+ * @link       https://github.com/EvgeniyBlinov/BashUtilities for the canonical source repository
+ * @author     Evgeniy Blinov <evgeniy_blinov@mail.ru>
+ * @copyright  Copyright (c) 2014 Evgeniy Blinov
+ * @license    http://www.opensource.org/licenses/mit-license.php MIT License
+ */
 defined('SCRIPT_NAME') or define('SCRIPT_NAME', $argv[0]);
 $scriptStatus = 0;
 
